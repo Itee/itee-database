0d0c7ac060c456f895a03983e250641d66089971
Tristan VALCKE <valcketristan@gmail.com>
2018-07-23 21:51:08 +0200

    Builds

--- builds/itee-database.cjs.js
+++ builds/itee-database.cjs.js
@@ -14,11 +14,13 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 class TAbstractDatabase {
 
-    constructor ( driver, plugins = [], autoReconnectTimeout = 10000 ) {
+    constructor ( driver, application, router, plugins = [], autoReconnectTimeout = 10000 ) {
 
         this.routes = {};
 
         this._driver               = driver;
+        this._application          = application;
+        this._router               = router;
         this._plugins              = plugins;
         this._autoReconnectTimeout = autoReconnectTimeout;
         this._autoConnectionTimer  = null;
@@ -32,6 +34,7 @@ class TAbstractDatabase {
         // Register modules plugins
         const pluginsNames = this._plugins;
         for ( let index = 0, numberOfPlugins = pluginsNames.length ; index < numberOfPlugins ; index++ ) {
+
             const pluginName = pluginsNames[ index ];
             let plugin       = undefined;
 
@@ -42,7 +45,8 @@ class TAbstractDatabase {
                 continue
             }
 
-            this.__registerPlugin( plugin );
+            plugin.registerTo( this._driver, this._application, this._router );
+
         }
 
         this._init();
@@ -52,13 +56,6 @@ class TAbstractDatabase {
     _init () {
 
         console.error( 'TAbstractDatabase._init: Need to be reimplemented in inherited class !' );
-
-    }
-
-    __registerPlugin ( plugin ) {
-
-        plugin.registerTo( this._driver );
-        this.routes = plugin.addRoutesTo( this.routes );
 
     }
 
@@ -897,6 +894,275 @@ class TAbstractDataController {
     _deleteAll ( response ) {}
 
 }
+
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Todo
+ *
+ * @example Todo
+ *
+ */
+
+const fs           = require( 'fs' );
+const { Writable } = require( 'stream' );
+const globalBuffer = require( 'buffer' );
+
+////////
+
+/* Writable memory stream */
+class MemoryWriteStream extends Writable {
+
+    constructor ( options ) {
+
+        super( options );
+
+        const bufferSize  = options.bufferSize || globalBuffer.kStringMaxLength;
+        this.memoryBuffer = Buffer.alloc( bufferSize );
+        this.offset       = 0;
+    }
+
+    _write ( chunk, encoding, callback ) {
+
+        // our memory store stores things in buffers
+        const buffer = (Buffer.isBuffer( chunk )) ? chunk : new Buffer( chunk, encoding );
+
+        // concat to the buffer already there
+        for ( let byteIndex = 0, numberOfByte = buffer.length ; byteIndex < numberOfByte ; byteIndex++ ) {
+            this.memoryBuffer[ this.offset ] = buffer[ byteIndex ];
+            this.offset++;
+        }
+
+        // Next
+        callback();
+
+    }
+
+    _writev ( chunks, callback ) {
+
+        for ( let chunkIndex = 0, numberOfChunks = chunks.length ; chunkIndex < numberOfChunks ; chunkIndex++ ) {
+            this.memoryBuffer = Buffer.concat( [ this.memoryBuffer, chunks[ chunkIndex ] ] );
+        }
+
+        // Next
+        callback();
+
+    }
+
+    _final ( callback ) {
+
+        callback();
+
+    }
+
+    _releaseMemory () {
+
+        this.memoryBuffer = null;
+
+    }
+
+    ////
+
+    toArrayBuffer () {
+
+        const buffer      = this.memoryBuffer;
+        const arrayBuffer = new ArrayBuffer( buffer.length );
+        const view        = new Uint8Array( arrayBuffer );
+
+        for ( let i = 0 ; i < buffer.length ; ++i ) {
+            view[ i ] = buffer[ i ];
+        }
+
+        this._releaseMemory();
+
+        return arrayBuffer
+
+    }
+
+    toString () {
+
+        const string = this.memoryBuffer.toString();
+        this._releaseMemory();
+
+        return string
+
+    }
+
+    toJSON () {
+
+        return JSON.parse( this.toString() )
+
+    }
+
+}
+
+////////
+
+function TAbstractFileConverter () {
+
+    this.dumpType = 'arraybuffer'; // 'string', 'json'
+
+    this._isProcessing = false;
+    this._filesQueue   = [];
+    this._fileData     = undefined;
+
+}
+
+TAbstractFileConverter.MAX_FILE_SIZE = 67108864;
+
+TAbstractFileConverter.prototype.convert = function convert ( file, parameters, onSuccess, onProgress, onError ) {
+
+    this._filesQueue.push( {
+        file,
+        parameters,
+        onSuccess,
+        onProgress,
+        onError
+    } );
+
+    if ( !this._isProcessing ) {
+        this._processQueue();
+    }
+
+};
+
+TAbstractFileConverter.prototype._processQueue = function _processQueue () {
+
+    if ( this._filesQueue.length === 0 ) {
+
+        this._isProcessing = false;
+        return
+
+    }
+
+    this._isProcessing = true;
+
+    const self              = this;
+    const fileData          = this._filesQueue.shift();
+    const currentFile       = fileData.file;
+    const currentParameters = fileData.parameters;
+    const currentOnSuccess  = fileData.onSuccess;
+    const currentOnProgress = fileData.onProgress;
+    const currentOnError    = fileData.onError;
+
+    self._dumpFileInMemoryAs(
+        this.dumpType,
+        currentFile,
+        currentParameters,
+        _onDumpSuccess,
+        _onProcessProgress,
+        _onProcessError
+    );
+
+    function _onDumpSuccess ( data ) {
+
+        self._fileData = data;
+        self._convert(
+            currentParameters,
+            _onProcessSuccess,
+            _onProcessProgress,
+            _onProcessError
+        );
+
+    }
+
+    function _onProcessSuccess ( threeData ) {
+
+        self._releaseMemory();
+        currentOnSuccess( threeData );
+        self._processQueue();
+
+    }
+
+    function _onProcessProgress ( progress ) {
+
+        currentOnProgress( progress );
+
+    }
+
+    function _onProcessError ( error ) {
+
+        currentOnError( error );
+        self._processQueue();
+
+    }
+
+};
+
+TAbstractFileConverter.prototype._dumpFileInMemoryAs = function ( dumpType, file, parameters, onSuccess, onProgress, onError ) {
+
+    let isOnError = false;
+
+    const fileReadStream = fs.createReadStream( file );
+
+    fileReadStream.on( 'error', ( error ) => {
+        console.error( `Read stream on error: ${error}` );
+
+        isOnError = true;
+        onError( error );
+
+    } );
+
+    const fileSize          = parseInt(parameters.fileSize);
+    const memoryWriteStream = new MemoryWriteStream( { bufferSize: fileSize } );
+
+    memoryWriteStream.on( 'error', ( error ) => {
+
+        isOnError = true;
+        onError( error );
+
+    } );
+
+    memoryWriteStream.on( 'finish', () => {
+
+        if ( isOnError ) {
+            return
+        }
+
+        switch ( dumpType ) {
+
+            case 'arraybuffer':
+                onSuccess( memoryWriteStream.toArrayBuffer() );
+                break
+
+            case 'string':
+                onSuccess( memoryWriteStream.toString() );
+                break
+
+            case 'json':
+                onSuccess( memoryWriteStream.toJSON() );
+                break
+
+            default:
+                throw new RangeError( `Invalid switch parameter: ${dumpType}` )
+                break
+
+        }
+
+        fileReadStream.unpipe();
+        fileReadStream.close();
+        memoryWriteStream.end();
+
+    } );
+
+    fileReadStream.pipe( memoryWriteStream );
+
+};
+
+TAbstractFileConverter.prototype._releaseMemory = function _releaseMemory () {
+
+    this._fileData = null;
+
+};
+
+TAbstractFileConverter.prototype._convert = function _convert ( parameters, onSuccess, onProgress, onError ) {
+
+    console.error( '_convert: Need to be reimplemented in inherited class !' );
+
+};
+
+module.exports = TAbstractFileConverter;
 
 /**
  * @author [Tristan Valcke]{@link https://github.com/Itee}
