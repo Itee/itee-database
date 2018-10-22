14617104fc2c59e97be4dc8e6d584ccfdc6d8fdf
Tristan VALCKE <valcketristan@gmail.com>
2018-10-22 10:17:14 +0200

    TAbstractDataController: Implement return methods to auto dispatch error and data to client ( in a i-return way that is now removed )

--- sources/controllers/TAbstractDataController.js
+++ sources/controllers/TAbstractDataController.js
@@ -22,14 +22,12 @@ const {
           isFunction
       } = require( 'itee-validators' )
 
-const I = require( 'i-return' )
-
 class TAbstractDataController {
 
     constructor ( driver, options ) {
 
         this._driver  = driver
-        this._options = Object.assign({
+        this._options = Object.assign( {
             useNext: false
         }, options )
 
@@ -64,7 +62,7 @@ class TAbstractDataController {
 
         } else {
 
-            I.returnError( {
+            TAbstractDataController.returnError( {
                 title:   'Erreur de paramètre',
                 message: `${dataName} n'existe pas dans les paramètres !`
             }, response )
@@ -144,25 +142,217 @@ class TAbstractDataController {
 
     }
 
+    ////////////
+    // Return //
+    ////////////
+
+    /**
+     * In case database call return nothing consider that is a not found.
+     * If response parameter is a function consider this is a returnNotFound callback function to call,
+     * else check if server response headers aren't send yet, and return response with status 204
+     *
+     * @param response - The server response or returnNotFound callback
+     * @returns {*} callback call or response with status 204
+     */
+    static returnNotFound ( response ) {
+
+        if ( isFunction( response ) ) { return response() }
+        if ( response.headersSent ) { return }
+
+        response.status( 204 ).end()
+
+    }
+
+    /**
+     * In case database call return an error.
+     * If response parameter is a function consider this is a returnError callback function to call,
+     * else check if server response headers aren't send yet, log and flush stack trace (if allowed) and return response with status 500 and
+     * stringified error as content
+     *
+     * @param error - A server/database error
+     * @param response - The server response or returnError callback
+     * @returns {*} callback call or response with status 500 and associated error
+     */
+    static returnError ( error, response ) {
+
+        if ( isFunction( response ) ) { return response( error, null ) }
+        if ( response.headersSent ) { return }
+
+        const formatedError = TAbstractDataController._formatError( error )
+
+        response.format( {
+
+            'application/json': () => {
+                response.status( 500 ).json( formatedError )
+            },
+
+            'default': () => {
+                response.status( 406 ).send( 'Not Acceptable' )
+            }
+
+        } )
+
+    }
+
+    /**
+     * In case database call return some data.
+     * If response parameter is a function consider this is a returnData callback function to call,
+     * else check if server response headers aren't send yet, and return response with status 200 and
+     * stringified data as content
+     *
+     * @param data - The server/database data
+     * @param response - The server response or returnData callback
+     * @returns {*} callback call or response with status 200 and associated data
+     */
+    static returnData ( data, response ) {
+
+        if ( isFunction( response ) ) { return response( null, data ) }
+        if ( response.headersSent ) { return }
+
+        response.format( {
+
+            'application/json': () => {
+                response.status( 200 ).json( data )
+            },
+
+            'default': () => {
+                response.status( 406 ).send( 'Not Acceptable' )
+            }
+
+        } )
+
+    }
+
+    /**
+     * In case database call return some data AND error.
+     * If response parameter is a function consider this is a returnErrorAndData callback function to call,
+     * else check if server response headers aren't send yet, log and flush stack trace (if allowed) and
+     * return response with status 406 with stringified data and error in a literal object as content
+     *
+     * @param error - A server/database error
+     * @param data - The server/database data
+     * @param response - The server response or returnErrorAndData callback
+     * @returns {*} callback call or response with status 406, associated error and data
+     */
+    static returnErrorAndData ( error, data, response ) {
+
+        if ( isFunction( response ) ) { return response( error, data ) }
+        if ( response.headersSent ) { return }
+
+        const result = {
+            errors: error,
+            datas:  data
+        }
+
+        response.format( {
+
+            'application/json': () => {
+                response.status( 520 ).json( result )
+            },
+
+            'default': () => {
+                response.status( 406 ).send( 'Not Acceptable' )
+            }
+
+        } )
+
+    }
+
+    return ( response, callbacks = {} ) {
+
+        const _callbacks = Object.assign( {
+
+                immediate:                null,
+                beforeAll:                null,
+                beforeReturnErrorAndData: null,
+                afterReturnErrorAndData:  null,
+                beforeReturnError:        null,
+                afterReturnError:         null,
+                beforeReturnData:         null,
+                afterReturnData:          null,
+                beforeReturnNotFound:     null,
+                afterReturnNotFound:      null,
+                afterAll:                 null
+
+            },
+            callbacks,
+            {
+                returnErrorAndData: TAbstractDataController.returnErrorAndData.bind( this ),
+                returnError:        TAbstractDataController.returnError.bind( this ),
+                returnData:         TAbstractDataController.returnData.bind( this ),
+                returnNotFound:     TAbstractDataController.returnNotFound.bind( this )
+            } )
+
+        /**
+         * The callback that will be used for parse database response
+         */
+        function dispatchResult ( error = null, data = null ) {
+
+            const haveData  = isDefined( data )
+            const haveError = isDefined( error )
+
+            if ( _callbacks.beforeAll ) { _callbacks.beforeAll() }
+
+            if ( haveData && haveError ) {
+
+                if ( _callbacks.beforeReturnErrorAndData ) { _callbacks.beforeReturnErrorAndData( error, data ) }
+                _callbacks.returnErrorAndData( error, data, response )
+                if ( _callbacks.afterReturnErrorAndData ) { _callbacks.afterReturnErrorAndData( error, data ) }
+
+            } else if ( haveData && !haveError ) {
+
+                if ( _callbacks.beforeReturnData ) { _callbacks.beforeReturnData( data ) }
+                _callbacks.returnData( data, response )
+                if ( _callbacks.afterReturnData ) { _callbacks.afterReturnData( data ) }
+
+            } else if ( !haveData && haveError ) {
+
+                if ( _callbacks.beforeReturnError ) { _callbacks.beforeReturnError( error ) }
+                _callbacks.returnError( error, response )
+                if ( _callbacks.afterReturnError ) { _callbacks.afterReturnError( error ) }
+
+            } else if ( !haveData && !haveError ) {
+
+                if ( _callbacks.beforeReturnNotFound ) { _callbacks.beforeReturnNotFound() }
+                _callbacks.returnNotFound( response )
+                if ( _callbacks.afterReturnNotFound ) { _callbacks.afterReturnNotFound() }
+
+            }
+
+            if ( _callbacks.afterAll ) { _callbacks.afterAll() }
+
+        }
+
+        // An immediate callback hook ( for timing for example )
+        if ( _callbacks.immediate ) { _callbacks.immediate() }
+
+        return dispatchResult
+
+    }
+
+    //////////////////
+    // CRUD Methods //
+    //////////////////
+
     create ( request, response, next ) {
 
         const data = request.body
 
         if ( isNotDefined( data ) ) {
 
-            I.returnError( {
+            TAbstractDataController.returnError( {
                 title:   'Erreur de paramètre',
                 message: 'Le corps de la requete ne peut pas être null ou indefini.'
-            }, ( this._options.useNext ) ? next : response )
+            }, (this._options.useNext) ? next : response )
 
         } else if ( isArray( data ) ) {
 
             if ( isEmptyArray( data ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'Le tableau d\'objet de la requete ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -174,10 +364,10 @@ class TAbstractDataController {
 
             if ( isEmptyObject( data ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'L\'objet de la requete ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -187,10 +377,10 @@ class TAbstractDataController {
 
         } else {
 
-            I.returnError( {
+            TAbstractDataController.returnError( {
                 title:   'Erreur de paramètre',
                 message: 'Le type de donnée de la requete est invalide. Les paramètres valides sont objet ou un tableau d\'objet.'
-            }, ( this._options.useNext ) ? next : response )
+            }, (this._options.useNext) ? next : response )
 
         }
 
@@ -213,17 +403,17 @@ class TAbstractDataController {
 
             if ( isNotString( id ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'L\'identifiant devrait être une chaine de caractères.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyString( id ) || isBlankString( id ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'L\'identifiant ne peut pas être une chaine de caractères vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -235,17 +425,17 @@ class TAbstractDataController {
 
             if ( isNotArray( ids ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'Le tableau d\'identifiants devrait être un tableau de chaine de caractères.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyArray( ids ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'Le tableau d\'identifiants ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -257,17 +447,17 @@ class TAbstractDataController {
 
             if ( isNotObject( query ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'La requete devrait être un objet javascript.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyObject( query ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'La requete ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -302,26 +492,26 @@ class TAbstractDataController {
 
         if ( isNotDefined( update ) ) {
 
-            I.returnError( {
+            TAbstractDataController.returnError( {
                 title:   'Erreur de paramètre',
                 message: 'La mise à jour a appliquer ne peut pas être null ou indefini.'
-            }, ( this._options.useNext ) ? next : response )
+            }, (this._options.useNext) ? next : response )
 
         } else if ( isDefined( id ) ) {
 
             if ( isNotString( id ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'L\'identifiant devrait être une chaine de caractères.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyString( id ) || isBlankString( id ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'L\'identifiant ne peut pas être une chaine de caractères vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -333,17 +523,17 @@ class TAbstractDataController {
 
             if ( isNotArray( ids ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'Le tableau d\'identifiants devrait être un tableau de chaine de caractères.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyArray( ids ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'Le tableau d\'identifiants ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -355,17 +545,17 @@ class TAbstractDataController {
 
             if ( isNotObject( query ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'La requete devrait être un objet javascript.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyObject( query ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'La requete ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -401,17 +591,17 @@ class TAbstractDataController {
 
             if ( isNotString( id ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'L\'identifiant devrait être une chaine de caractères.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyString( id ) || isBlankString( id ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'L\'identifiant ne peut pas être une chaine de caractères vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -423,17 +613,17 @@ class TAbstractDataController {
 
             if ( isNotArray( ids ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'Le tableau d\'identifiants devrait être un tableau de chaine de caractères.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyArray( ids ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'Le tableau d\'identifiants ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
@@ -445,17 +635,17 @@ class TAbstractDataController {
 
             if ( isNotObject( query ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'La requete devrait être un objet javascript.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else if ( isEmptyObject( query ) ) {
 
-                I.returnError( {
+                TAbstractDataController.returnError( {
                     title:   'Erreur de paramètre',
                     message: 'La requete ne peut pas être vide.'
-                }, ( this._options.useNext ) ? next : response )
+                }, (this._options.useNext) ? next : response )
 
             } else {
 
