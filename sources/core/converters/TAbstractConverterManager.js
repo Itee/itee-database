e45c39f1cdfc693948eb0c075545208118154dac
Tristan VALCKE <valcketristan@gmail.com>
2019-07-29 16:35:12 +0200

    refactor(tabstractconvertermanager): remove i-return dependency in favor of local return methods
    This commit allow to remove the i-return package in favor of local return management

--- sources/core/converters/TAbstractConverterManager.js
+++ sources/core/converters/TAbstractConverterManager.js
@@ -8,11 +8,89 @@
  *
  */
 
-import * as I from 'i-return'
-import path   from 'path'
+import {
+    isArray,
+    isDefined,
+    isFunction,
+    isObject,
+    isString
+} from 'itee-validators'
+import path          from 'path'
 
 // Todo: Extend sort of Factory
 class TAbstractConverterManager {
+
+    /**
+     * Normalize error that can be in different format like single string, object, array of string, or array of object.
+     *
+     * @example <caption>Normalized error are simple literal object like:</caption>
+     * {
+     *     title: 'error',
+     *     message: 'the error message'
+     * }
+     *
+     * @param {String|Object|Array.<String>|Array.<Object>} error - The error object to normalize
+     * @returns {Array.<Object>}
+     * @private
+     */
+    static _formatError ( error ) {
+        let errorsList = []
+
+        if ( isArray( error ) ) {
+
+            for ( let i = 0, l = error.length ; i < l ; ++i ) {
+                errorsList = errorsList.concat( TAbstractDataController._formatError( error[ i ] ) )
+            }
+
+        } else if ( isObject( error ) ) {
+
+            if ( error.name === 'ValidationError' ) {
+
+                let _message  = ''
+                let subsError = error.errors
+
+                for ( let property in subsError ) {
+                    if ( !subsError.hasOwnProperty( property ) ) { continue }
+                    _message += subsError[ property ].message + '<br>'
+                }
+
+                errorsList.push( {
+                    title:   'Erreur de validation',
+                    message: _message || 'Aucun message d\'erreur... Gloups !'
+                } )
+
+            } else if ( error.name === 'VersionError' ) {
+
+                errorsList.push( {
+                    title:   'Erreur de base de donnée',
+                    message: 'Aucun document correspondant n\'as put être trouvé pour la requete !'
+                } )
+
+            } else {
+
+                errorsList.push( {
+                    title:   error.title || 'Erreur',
+                    message: error.message || 'Aucun message d\'erreur... Gloups !'
+                } )
+
+            }
+
+        } else if ( isString( error ) ) {
+
+            errorsList.push( {
+                title:   'Erreur',
+                message: error
+            } )
+
+        } else {
+
+            throw new Error( `Unknown error type: ${error} !` )
+
+        }
+
+        return errorsList
+
+    }
 
     static _convertFilesObjectToArray ( files ) {
 
@@ -31,6 +109,193 @@ class TAbstractConverterManager {
         return fileArray
 
     }
+
+    /**
+     * In case database call return nothing consider that is a not found.
+     * If response parameter is a function consider this is a returnNotFound callback function to call,
+     * else check if server response headers aren't send yet, and return response with status 204
+     *
+     * @param response - The server response or returnNotFound callback
+     * @returns {*} callback call or response with status 204
+     */
+    static returnNotFound ( response ) {
+
+        if ( isFunction( response ) ) { return response() }
+        if ( response.headersSent ) { return }
+
+        response.status( 204 ).end()
+
+    }
+
+    /**
+     * In case database call return an error.
+     * If response parameter is a function consider this is a returnError callback function to call,
+     * else check if server response headers aren't send yet, log and flush stack trace (if allowed) and return response with status 500 and
+     * stringified error as content
+     *
+     * @param error - A server/database error
+     * @param response - The server response or returnError callback
+     * @returns {*} callback call or response with status 500 and associated error
+     */
+    static returnError ( error, response ) {
+
+        if ( isFunction( response ) ) { return response( error, null ) }
+        if ( response.headersSent ) { return }
+
+        const formatedError = TAbstractConverterManager._formatError( error )
+
+        response.format( {
+
+            'application/json': () => {
+                response.status( 500 ).json( formatedError )
+            },
+
+            'default': () => {
+                response.status( 406 ).send( 'Not Acceptable' )
+            }
+
+        } )
+
+    }
+
+    /**
+     * In case database call return some data.
+     * If response parameter is a function consider this is a returnData callback function to call,
+     * else check if server response headers aren't send yet, and return response with status 200 and
+     * stringified data as content
+     *
+     * @param data - The server/database data
+     * @param response - The server response or returnData callback
+     * @returns {*} callback call or response with status 200 and associated data
+     */
+    static returnData ( data, response ) {
+
+        if ( isFunction( response ) ) { return response( null, data ) }
+        if ( response.headersSent ) { return }
+
+        const _data = isArray( data ) ? data : [ data ]
+
+        response.format( {
+
+            'application/json': () => {
+                response.status( 200 ).json( _data )
+            },
+
+            'default': () => {
+                response.status( 406 ).send( 'Not Acceptable' )
+            }
+
+        } )
+
+    }
+
+    /**
+     * In case database call return some data AND error.
+     * If response parameter is a function consider this is a returnErrorAndData callback function to call,
+     * else check if server response headers aren't send yet, log and flush stack trace (if allowed) and
+     * return response with status 406 with stringified data and error in a literal object as content
+     *
+     * @param error - A server/database error
+     * @param data - The server/database data
+     * @param response - The server response or returnErrorAndData callback
+     * @returns {*} callback call or response with status 406, associated error and data
+     */
+    static returnErrorAndData ( error, data, response ) {
+
+        if ( isFunction( response ) ) { return response( error, data ) }
+        if ( response.headersSent ) { return }
+
+        const result = {
+            errors: error,
+            datas:  data
+        }
+
+        response.format( {
+
+            'application/json': () => {
+                response.status( 416 ).json( result )
+            },
+
+            'default': () => {
+                response.status( 416 ).send( 'Range Not Satisfiable' )
+            }
+
+        } )
+
+    }
+
+    static return ( response, callbacks = {} ) {
+
+        const _callbacks = Object.assign( {
+
+                immediate:                null,
+                beforeAll:                null,
+                beforeReturnErrorAndData: null,
+                afterReturnErrorAndData:  null,
+                beforeReturnError:        null,
+                afterReturnError:         null,
+                beforeReturnData:         null,
+                afterReturnData:          null,
+                beforeReturnNotFound:     null,
+                afterReturnNotFound:      null,
+                afterAll:                 null
+
+            },
+            callbacks,
+            {
+                returnErrorAndData: TAbstractConverterManager.returnErrorAndData.bind( this ),
+                returnError:        TAbstractConverterManager.returnError.bind( this ),
+                returnData:         TAbstractConverterManager.returnData.bind( this ),
+                returnNotFound:     TAbstractConverterManager.returnNotFound.bind( this )
+            } )
+
+        /**
+         * The callback that will be used for parse database response
+         */
+        function dispatchResult ( error = null, data = null ) {
+
+            const haveData  = isDefined( data )
+            const haveError = isDefined( error )
+
+            if ( _callbacks.beforeAll ) { _callbacks.beforeAll() }
+
+            if ( haveData && haveError ) {
+
+                if ( _callbacks.beforeReturnErrorAndData ) { _callbacks.beforeReturnErrorAndData( error, data ) }
+                _callbacks.returnErrorAndData( error, data, response )
+                if ( _callbacks.afterReturnErrorAndData ) { _callbacks.afterReturnErrorAndData( error, data ) }
+
+            } else if ( haveData && !haveError ) {
+
+                if ( _callbacks.beforeReturnData ) { _callbacks.beforeReturnData( data ) }
+                _callbacks.returnData( data, response )
+                if ( _callbacks.afterReturnData ) { _callbacks.afterReturnData( data ) }
+
+            } else if ( !haveData && haveError ) {
+
+                if ( _callbacks.beforeReturnError ) { _callbacks.beforeReturnError( error ) }
+                _callbacks.returnError( error, response )
+                if ( _callbacks.afterReturnError ) { _callbacks.afterReturnError( error ) }
+
+            } else if ( !haveData && !haveError ) {
+
+                if ( _callbacks.beforeReturnNotFound ) { _callbacks.beforeReturnNotFound() }
+                _callbacks.returnNotFound( response )
+                if ( _callbacks.afterReturnNotFound ) { _callbacks.afterReturnNotFound() }
+
+            }
+
+            if ( _callbacks.afterAll ) { _callbacks.afterAll() }
+
+        }
+
+        // An immediate callback hook ( for timing for example )
+        if ( _callbacks.immediate ) { _callbacks.immediate() }
+
+        return dispatchResult
+
+    }
+
 
     constructor ( parameters = {} ) {
 
@@ -100,7 +365,7 @@ class TAbstractConverterManager {
             if ( this._useNext ) {
                 next( this._errors )
             } else {
-                I.return( response )( this._errors )
+                TAbstractConverterManager.return( response )( this._errors )
                 this._errors = []
             }
 
@@ -108,7 +373,7 @@ class TAbstractConverterManager {
             if ( this._useNext ) {
                 next()
             } else {
-                I.returnData( data, response )
+                TAbstractConverterManager.returnData( data, response )
             }
         }
 
@@ -131,7 +396,7 @@ class TAbstractConverterManager {
             if ( this._useNext ) {
                 next( `Impossible d'analyser ${numberOfFiles} fichiers associatifs simultanément !` )
             } else {
-                I.returnError( `Impossible d'analyser ${numberOfFiles} fichiers associatifs simultanément !`, response )
+                TAbstractConverterManager.returnError( `Impossible d'analyser ${numberOfFiles} fichiers associatifs simultanément !`, response )
             }
 
         }
