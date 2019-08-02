8333f377497e47bc53c7d84a66d8b2b9b9cd0d87
Tristan VALCKE <valcketristan@gmail.com>
2019-08-02 23:56:20 +0200

    build(rollupconfig): update rollup config file to latest itee standard

--- configs/rollup.conf.js
+++ configs/rollup.conf.js
@@ -2,146 +2,150 @@
  * @author [Tristan Valcke]{@link https://github.com/Itee}
  * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
  *
- * @module config/rollupConfiguration
- *
+ * @module Config-Rollup
  * @description The file manage the rollup configuration for build the library using differents arguments. It allow to build with two type of environment (dev and prod), and differents output format.
  * Use npm run help to display all available build options.
  *
  * @requires {@link module: [path]{@link https://nodejs.org/api/path.html}}
- * @requires {@link module: [rollup-plugin-buble]{@link https://github.com/rollup/rollup-plugin-buble}}
+ * @requires {@link module: [rollup-plugin-babel]{@link https://github.com/rollup/rollup-plugin-babel}}
  * @requires {@link module: [rollup-plugin-node-resolve]{@link https://github.com/rollup/rollup-plugin-node-resolve}}
  * @requires {@link module: [rollup-plugin-commonjs]{@link https://github.com/rollup/rollup-plugin-commonjs}}
  * @requires {@link module: [rollup-plugin-replace]{@link https://github.com/rollup/rollup-plugin-replace}}
- * @requires {@link module: [rollup-plugin-uglify-es]{@link https://github.com/TrySound/rollup-plugin-uglify}}
+ * @requires {@link module: [rollup-plugin-terser]{@link https://github.com/TrySound/rollup-plugin-terser}}
  *
  */
 
 /* eslint-env node */
 
-const path        = require( 'path' )
-const babel       = require( 'rollup-plugin-babel' )
-const nodeResolve = require( 'rollup-plugin-node-resolve' )
-const commonJs    = require( 'rollup-plugin-commonjs' )
-const replace     = require( 'rollup-plugin-replace' )
-const uglify      = require( 'rollup-plugin-uglify-es' )
-const json        = require( 'rollup-plugin-json' )
+const path     = require( 'path' )
+const resolve  = require( 'rollup-plugin-node-resolve' )
+const commonjs = require( 'rollup-plugin-commonjs' )
+const replace  = require( 'rollup-plugin-re' )
+const json     = require( 'rollup-plugin-json' )
+const terser   = require( 'rollup-plugin-terser' ).terser
 
 /**
  * Will create an appropriate configuration object for rollup, related to the given arguments.
  *
- *
- * @param fileName
- * @param inputPath
- * @param outputPath
- * @param {string} format - The desired module format output. Available values are: 'es', 'cjs', 'amd', 'iife' and 'umd'
- * @param {boolean} onProduction - The building environment. True for production, developement otherwise.
- * @param {boolean} wantSourceMap - Set to true if sourcemap must be include in the output.
- * @returns {object} The rollup configuration
+ * @generator
+ * @param options
+ * @return {Array.<json>} An array of rollup configuration
  */
-function CreateRollupConfiguration ( fileName, inputPath, outputPath, format, onProduction, wantSourceMap ) {
+function CreateRollupConfigs ( options ) {
+    'use strict'
 
-    const _format        = format || 'umd'
-    const _onProduction  = onProduction || false
-    const _wantSourceMap = wantSourceMap || false
+    const name      = options.name
+    const input     = options.input
+    const output    = options.output
+    const formats   = options.format.split( ',' )
+    const envs      = options.env.split( ',' )
+    const sourcemap = options.sourcemap
+    const treeshake = options.treeshake
+    const fileName  = path.basename( input, '.js' )
 
-    const fileExtension  = ( _onProduction ) ? '.min.js' : '.js'
-    const inputFilePath  = path.join( inputPath, fileName + '.js' )
-    const outputFilePath = path.join( outputPath, fileName + '.' + _format + fileExtension )
+    const configs = []
 
-    return {
-        inputOptions:  {
+    for ( let formatIndex = 0, numberOfFormats = formats.length ; formatIndex < numberOfFormats ; ++formatIndex ) {
 
-            // core options
-            input:    inputFilePath,
-            external: [
-                'path',
-                'fs',
-                'net',
-                'url',
-                'tty',
-                'util',
-                'events',
-                'zlib',
-                'os',
-                'dns',
-                'stream',
-                'crypto',
-                'assert',
-                'querystring',
-                'http',
-                'child_process',
-                'https',
-                'punycode',
-                'buffer',
-                'timers',
-                'vm',
-                'tls',
-                'constants',
-                'string_decoder',
-                'async_hooks',
-                'hiredis',
-                'dgram',
-                'pg-native'
-            ],
-            plugins:  [
-                commonJs( {
-                    include: 'node_modules/**'
-                } ),
-                babel( require( './babel.conf' )( _onProduction ) ),
-//                replace( {
-//                    'global.process.env.NODE_ENV': JSON.stringify( ( _onProduction ) ? 'production' : 'development' ),
-//                    'process.env.NODE_ENV': JSON.stringify( ( _onProduction ) ? 'production' : 'development' )
-//                } ),
-                json({}),
-                nodeResolve( {
-                    preferBuiltins: true
-                } ),
-                onProduction && uglify()
-            ],
+        for ( let envIndex = 0, numberOfEnvs = envs.length ; envIndex < numberOfEnvs ; envIndex++ ) {
 
-            // advanced options
-            onwarn: function onWarn ( { loc, frame, message } ) {
-                if ( loc ) {
-                    process.stderr.write( `/!\\ WARNING: ${loc.file} (${loc.line}:${loc.column}) ${frame} ${message}\n` )
-                } else {
-                    process.stderr.write( `/!\\ WARNING: ${message}\n` )
+            const env        = envs[ envIndex ]
+            const prod       = ( env.includes( 'prod' ) )
+            const format     = formats[ formatIndex ]
+            const outputPath = ( prod ) ? path.join( output, `${fileName}.${format}.min.js` ) : path.join( output, `${fileName}.${format}.js` )
+
+            configs.push( {
+                input:    input,
+                external: ( [ 'esm', 'cjs' ].includes( format ) ) ? [
+                    'assert',
+                    'async_hooks',
+                    'buffer',
+                    'child_process',
+                    'constants',
+                    'crypto',
+                    'dgram',
+                    'dns',
+                    'events',
+                    'fs',
+                    'hiredis',
+                    'http',
+                    'https',
+                    'module',
+                    'net',
+                    'os',
+                    'path',
+                    'pg-native',
+                    'punycode',
+                    'querystring',
+                    'stream',
+                    'string_decoder',
+                    'timers',
+                    'tls',
+                    'tty',
+                    'url',
+                    'util',
+                    'vm',
+                    'zlib'
+                ] : [],
+                plugins: [
+                    replace( {
+                        defines: {
+                            IS_REMOVE: prod
+                        }
+                    } ),
+                    commonjs( {
+                        include: 'node_modules/**'
+                    } ),
+                    resolve( {
+                        preferBuiltins: true
+                    } ),
+                    json( {} ),
+                    prod && terser()
+                ],
+                onwarn: ( { loc, frame, message } ) => {
+
+                    // Ignore some errors
+                    if ( message.includes( 'Circular dependency' ) ) { return }
+                    if ( message.includes( 'plugin uglify is deprecated' ) ) { return }
+
+                    if ( loc ) {
+                        process.stderr.write( `/!\\ ${loc.file} (${loc.line}:${loc.column}) ${frame} ${message}\n` )
+                    } else {
+                        process.stderr.write( `/!\\ ${message}\n` )
+                    }
+                },
+                treeshake: treeshake,
+                output:    {
+                    // core options
+                    file:    outputPath,
+                    format:  format,
+                    name:    name,
+                    globals: {},
+
+                    // advanced options
+                    paths:     {},
+                    banner:    '',
+                    footer:    '',
+                    intro:     '',
+                    outro:     '',
+                    sourcemap: sourcemap,
+                    interop:   true,
+
+                    // danger zone
+                    exports: 'auto',
+                    amd:     {},
+                    indent:  '\t',
+                    strict:  true
                 }
-            },
-            cache:  undefined,
+            } )
 
-            // danger zone
-            treeshake:     false,
-            acorn:         undefined,
-            context:       undefined,
-            moduleContext: {}
-        },
-        outputOptions: {
-            // core options
-            file:    outputFilePath,
-            format:  format,
-            name:    'Itee',
-            globals: {
-                //                'three': 'THREE'
-            },
-
-            // advanced options
-            paths:     {},
-            banner:    '',
-            footer:    '',
-            intro:     '',
-            outro:     '',
-            sourcemap: _wantSourceMap,
-            interop:   true,
-
-            // danger zone
-            exports: 'auto',
-            amd:     {},
-            indent:  '  ',
-            strict:  true
         }
 
     }
 
+    return configs
+
 }
 
-module.exports = CreateRollupConfiguration
+module.exports = CreateRollupConfigs
+
