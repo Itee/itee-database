9126676c28828c39686a2674034c59c553d3df28
Tristan VALCKE <valcketristan@gmail.com>
2025-10-25 10:31:24 +0200

    :wrench: (build) fix @rollup/plugin-terser dependency import

--- .tasks/configs/build.conf.mjs
+++ .tasks/configs/build.conf.mjs
@@ -14,13 +14,13 @@
  */
 import commonjs    from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
+import terser      from '@rollup/plugin-terser'
 import figlet      from 'figlet'
 import {
     basename,
     join
 }                  from 'path'
 import cleanup     from 'rollup-plugin-cleanup'
-import { terser }  from 'rollup-plugin-terser'
 import {
     getPrettyPackageName,
     getPrettyPackageVersion,
